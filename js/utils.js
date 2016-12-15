e8473b3bbd5ea2a70be8a2d6307000c24cdb19f1
Beskhue <thomas@kepow.org>
2016-12-15 01:47:11 +0100

    Add hidden functionality to generate new save-games incorporating the calculator's suggested changes.

--- js/utils.js
+++ js/utils.js
@@ -42,3 +42,73 @@ function numberToStringFormatted(number) {
         return addCommas(number);
     }
 }
+
+
+/* Encoding and decoding save games */
+var ANTI_CHEAT_CODE = "Fe12NAfA3R6z4k0z";
+var SALT = "af0ik392jrmt0nsfdghy0";
+var CHARACTERS = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+function decodeSaveGame(txtIn) {
+    var txtOut="";
+
+    if (txtIn.search("ClickerHeroesAccountSO") != -1) {
+        var start = 53;
+        txtOut = txtIn.substr(start, txtIn.length - start - 1);
+    } else {
+        txtIn = txtIn.trim();
+        if (txtIn=="") {
+            return;
+        }
+        if (txtIn.search(ANTI_CHEAT_CODE) != -1) {
+            var result = txtIn.split(ANTI_CHEAT_CODE);
+            for (var i = 0; i < result[0].length; i += 2) {
+                txtOut += result[0][i];
+            }
+            if (CryptoJS.MD5(txtOut + SALT) != result[1]) {
+                alert("Bad hash");
+                return;
+            }
+        }
+        txtOut = atob(txtOut);
+    }
+
+    try {
+        var rawData = $.parseJSON(txtOut);
+        return rawData;
+    } catch(e) {
+        alert('Could not decode the save game data.');
+        return null;
+    }
+}
+
+function encodeSaveGame(rawData) {
+    var json = JSON.stringify(rawData);
+    var base64String = btoa(json);
+    return sprinkle(base64String) + ANTI_CHEAT_CODE + getHash(base64String);
+}
+
+/* Thanks to:
+ * https://jsfiddle.net/A45327Eq/fr1vmz3x/
+ */
+function sprinkle(string) {
+    var characters;
+    var randomIndex;
+    var array = string.split("");
+    var result = [];
+    var counter = 0;
+    while (counter < array.length) {
+        result[counter * 2] = array[counter];
+        characters = CHARACTERS;
+        randomIndex = Math.floor(Math.random() * (characters.length - 1));
+        result[counter * 2 + 1] = characters.substr(randomIndex, 1);
+        counter++;
+    }
+    return result.join("");
+}
+
+function getHash(string) {
+    var charaters = string.split();
+    charaters.sort();
+    var sortedCharaters = charaters.join();
+    return CryptoJS.MD5(sortedCharaters + SALT);
+}
