8131955f140bebc40a4d2bdfac756f4c25233e7f
Beskhue <thomas@kepow.org>
2017-07-22 14:55:00 +0200

    Re-enable sprinkle-style encoding and decoding. Improve documentation.

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,7 @@
+/**********************************************************/
+/* Encoding and decoding algorithm configuration          */
+/**********************************************************/
+
 var hashesAlgos = {
     "7a990d405d2c6fb93aa8fbb0ec1a3b23": "zlib"
 };
@@ -11,6 +15,10 @@ var decodeAlgos = {
 var encodeAlgos = {
     "zlib": encodeZlib
 };
+
+/**********************************************************/
+/* Number formatting utilities                            */
+/**********************************************************/
 
 function numberToString(number, decimals) {
     // decimals = 3 default
@@ -43,8 +51,7 @@ function numberToClickerHeroesPasteableString(number, precision) {
     }
 }
 
-function addCommas(nStr)
-{
+function addCommas(nStr) {
     nStr += '';
     x = nStr.split('.');
     x1 = x[0];
@@ -68,8 +75,9 @@ function numberToStringFormatted(number, decimals) {
     }
 }
 
-
-/* Encoding and decoding save games */
+/**********************************************************/
+/* Encoding and decoding save games                       */
+/**********************************************************/
 
 /**
  * Decode a base-64 encoded string of some JSON encoding
@@ -88,7 +96,8 @@ function decodeSaveGame(str) {
     
     try {
         if (algo == "sprinkle") {
-            alert('Decoding sprinkle-style strings temporarily disabled.');
+            var json = decodeSprinkle(str);
+            return {data: $.parseJSON(json), algo: algo};
         } else {
             var strStripped = str.substring(32);
             
@@ -111,7 +120,7 @@ function decodeZlib(str) {
 function encodeSaveGame(rawData, algo) {
     // Special case old-style sprinkle encoding
     if (algo == "sprinkle") {
-        alert('Encoding data in sprinkle-style temporarily disabled.');
+        return encodeSprinkle(rawData);
     } else {
         if (!(algo in encodeAlgos)) {
             alert('Cannot encode using algo: ' + algo);
@@ -131,6 +140,81 @@ function encodeSaveGame(rawData, algo) {
 function encodeZlib(str) {
     return pako.deflate(str, {to: 'string'});
 }
+
+/**********************************************************/
+/* Old sprinkle-style encoding and decoding               */
+/**********************************************************/
+
+var ANTI_CHEAT_CODE = "Fe12NAfA3R6z4k0z";
+var SALT = "af0ik392jrmt0nsfdghy0";
+var CHARACTERS = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+function decodeSprinkle(txtIn) {
+    var txtOut="";
+
+    if (txtIn.search("ClickerHeroesAccountSO") != -1) {
+        var start = 53;
+        txtOut = txtIn.substr(start, txtIn.length - start - 1);
+    } else {
+        txtIn = txtIn.trim();
+        if (txtIn=="") {
+            return;
+        }
+        if (txtIn.search(ANTI_CHEAT_CODE) != -1) {
+            var result = txtIn.split(ANTI_CHEAT_CODE);
+            for (var i = 0; i < result[0].length; i += 2) {
+                txtOut += result[0][i];
+            }
+            if (CryptoJS.MD5(txtOut + SALT) != result[1]) {
+                alert("Bad hash");
+                return;
+            }
+        }
+        txtOut = atob(txtOut);
+    }
+
+    try {
+        return txtOut;
+    } catch(e) {
+        alert('Could not decode the save game data.');
+        return null;
+    }
+}
+
+function encodeSprinkle(rawData) {
+    var json = JSON.stringify(rawData);
+    var base64String = btoa(json);
+    return sprinkle(base64String) + ANTI_CHEAT_CODE + getHash(base64String);
+}
+
+/* Thanks to:
+ * https://jsfiddle.net/A45327Eq/fr1vmz3x/
+ */
+function sprinkle(string) {
+    var characters;
+    var randomIndex;
+    var array = string.split("");
+    var result = [];
+    var counter = 0;
+    while (counter < array.length) {
+        result[counter * 2] = array[counter];
+        characters = CHARACTERS;
+        randomIndex = Math.floor(Math.random() * (characters.length - 1));
+        result[counter * 2 + 1] = characters.substr(randomIndex, 1);
+        counter++;
+    }
+    return result.join("");
+}
+
+function getHash(string) {
+    var charaters = string.split();
+    charaters.sort();
+    var sortedCharaters = charaters.join();
+    return CryptoJS.MD5(sortedCharaters + SALT);
+}
+
+/**********************************************************/
+/* Generic utilities                                      */
+/**********************************************************/
 
 /**
  * Return arg if arg is defined, or val if arg is undefined.
